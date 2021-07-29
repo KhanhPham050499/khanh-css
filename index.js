c8c9b10ffc39e48870706ab82f4c9f2209fb7771
Khanh Pham <phamngocbaokhanh2015@gmail.com>
2021-07-29 17:08:42 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+export function colorjs(color) {
+    let div = document.getElementsByClassName('divColor')[0];
+
+    if (color === 'red') {
+        div.style.color = 'red';
+    }
+    if (color === 'blue') {
+        div.style.color = 'blue';
+    }
+}
